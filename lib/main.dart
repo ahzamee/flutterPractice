27676d8ad28a6d735c26575dd223ed38343777bf
zamee <hussain01673@gmail.com>
2020-04-13 15:29:40 +0600

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,37 @@ void main(){
   runApp(new Application());
 }
 
-class Application extends StatefulWidget {
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MyApplication",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Row & Column"),
+          backgroundColor: Colors.black, 
+        ),
+        body: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("this"),
+            new Text("is"),
+            new Text("zamee"),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text("new Column"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*class Application extends StatefulWidget {
   @override
   _applicationState createState() => _applicationState();
 }
@@ -30,12 +60,12 @@ class _applicationState extends State<Application> {
       title: "Statefull",
       home: new Scaffold(
         body: new Center(
-          child: new RaisedButton(onPressed: (){method();}, child: Text(str),),
+          child: new RaisedButton(onPressed: (){method();}, child: Text("Raised Button"),),
         ),
       ),
     );
   }
-}
+}*/
 
 
 /*class application extends StatelessWidget {
