b596335767b5b964b35bd99f6ceedc01953b447b
zamee <hussain01673@gmail.com>
2020-04-14 14:25:21 +0600

    ~~

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,136 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(new ListViewWidget());
+  runApp(new MaterialApp(home: new BasicAppBarApplication()));
 }
 
+class BasicAppBarApplication extends StatefulWidget {
+  @override
+  _BasicAppBarApplicationState createState() => _BasicAppBarApplicationState();
+}
+
+class _BasicAppBarApplicationState extends State<BasicAppBarApplication> with SingleTickerProviderStateMixin{
+
+  String mtext = '';
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = new TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.green,
+        title: new Text("AppBar Widget"),
+        leading: new Icon(Icons.menu),
+        elevation: 20.0,
+        toolbarOpacity: 0.8,
+        bottomOpacity: 0.5,
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.arrow_forward), onPressed: (){setState(() {
+            mtext = "this is Arrow icon button";
+          });}),
+          new IconButton(icon: new Icon(Icons.close), onPressed: (){setState(() {
+            mtext = "this is Close icon button";
+          });}),
+        ],
+
+        bottom: new TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.home),),
+            new Tab(icon: new Icon(Icons.supervisor_account),),
+            new Tab(icon: new Icon(Icons.details),),
+          ],
+        ),
+      ),
+      /*body: new Center( //only for appbar
+        child: new Text(mtext),
+      ),*/
+      body: new TabBarView(controller: _tabController,
+          children: <Widget>[
+            new Center(child: new Text("This is home Page"),),
+            new Center(child: new Text("This is User Account"),),
+            new Center(child: new Text("This is Details Page"),)
+          ],
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.blue,
+        child: new TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.home),),
+            new Tab(icon: new Icon(Icons.supervisor_account),),
+            new Tab(icon: new Icon(Icons.details),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*class GridViewApplication extends StatefulWidget {
+  @override
+  _GridViewApplicationState createState() => _GridViewApplicationState();
+}
+
+class _GridViewApplicationState extends State<GridViewApplication> {
+
+  List<int> items = new List();
+
+  @override
+  void initState() {
+    for(int i = 0; i<50; i++){
+      items.add(i);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new GridView.builder(
+          itemCount: items.length,
+          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (BuildContext context, int index){
+            return new Card(
+              color: Colors.blue,
+              child: new Text("values: $index"),
+            );
+          }),
+    );
+  }
+}*/
+
+/*
+class _GridViewApplicationState extends State<GridViewApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          new Card(color: Colors.yellow, child: new Padding(padding: const EdgeInsets.all(200.0),)),
+          new Card(color: Colors.red, child: new Padding(padding: const EdgeInsets.all(100.0),)),
+          new Card(color: Colors.green, child: new Padding(padding: const EdgeInsets.all(50.0),)),
+          new Card(color: Colors.blue, child: new Padding(padding: const EdgeInsets.all(30.0),)),
+          new Card(color: Colors.brown, child: new Padding(padding: const EdgeInsets.all(10.0),)),
+        ],
+      ),
+    );
+  }
+}
+*/
+
+
+/*
 class ListViewWidget extends StatefulWidget {
   @override
   _ListViewWidgetState createState() => _ListViewWidgetState();
@@ -23,7 +150,7 @@ class _ListViewWidgetState extends State<ListViewWidget> {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "Dummy Application",
+      title: "Dummy GridViewApplication",
       home: new Scaffold(
         appBar: new AppBar(title : new Text("ListView widget"),),
         body: new ListView.builder(
@@ -37,6 +164,7 @@ class _ListViewWidgetState extends State<ListViewWidget> {
     );
   }
 }
+*/
 
 
 /*class _ListViewWidgetState extends State<ListViewWidget> {
@@ -72,7 +200,7 @@ class _ListViewWidgetState extends State<ListViewWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "MyApplication",
+      title: "MyGridViewApplication",
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("Row & Column"),
@@ -97,12 +225,12 @@ class _ListViewWidgetState extends State<ListViewWidget> {
   }
 }*/
 
-/*class Application extends StatefulWidget {
+/*class GridViewApplication extends StatefulWidget {
   @override
   _applicationState createState() => _applicationState();
 }
 
-class _applicationState extends State<Application> {
+class _applicationState extends State<GridViewApplication> {
 
   String str = '';
   @override
